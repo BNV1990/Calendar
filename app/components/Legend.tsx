ffb338a3d14203fe46594e448cd37cc35e5ec280
BNV1990 <03nazar03@gmail.com>
2025-05-19 17:58:36 +0300

    Fix

--- app/components/Legend.tsx
+++ app/components/Legend.tsx
@@ -1,22 +1,12 @@
-import React, { useState } from "react";
-import { FaChevronDown } from "react-icons/fa"; // Import an arrow icon
+import React from "react";
 
 const Legend = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleAccordion = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <div className="legend accordion no-accordion-mobile">
-      <h3 className="accordion-header" onClick={toggleAccordion}>
+    <div className="legend">
+      <h3>
         Пояснення кольорів:
-        <span className={`accordion-icon ${isOpen ? "rotate" : ""}`}>
-          <FaChevronDown />
-        </span>
       </h3>
-      <div className={`accordion-content ${isOpen ? "open" : ""}`}>
+      <div>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           {/* Desktop view */}
           <div className="legend-desktop">
