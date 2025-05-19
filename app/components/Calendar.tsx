163279b5abda2e0e40aa7a45f6d884d287330bf6
BNV1990 <03nazar03@gmail.com>
2025-05-19 14:19:41 +0300

    Add https://swiperjs.com/react#effects

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css'; // Import Swiper styles
+import SwiperCore from 'swiper';
 
 interface CalendarProps {
   calendarRows: React.JSX.Element[];
@@ -16,40 +19,13 @@ const Calendar: React.FC<CalendarProps> = ({
   prevMonth,
   nextMonth,
 }) => {
-  const [touchStartX, setTouchStartX] = useState(0);
-  const [animationClass, setAnimationClass] = useState('');
 
-  const handleTouchStart = (e: React.TouchEvent) => {
-    setTouchStartX(e.touches[0].clientX);
-  };
-
-  const handleTouchMove = (e: React.TouchEvent) => {
-    // Prevent default to avoid scrolling while swiping
-    e.preventDefault();
-  };
-
-  const handleTouchEnd = (e: React.TouchEvent) => {
-    const touchEndX = e.changedTouches[0].clientX;
-    const swipeDistance = touchEndX - touchStartX;
-    const swipeThreshold = 50; // Minimum distance for a swipe
-
-    if (swipeDistance > swipeThreshold) {
-      setAnimationClass('slide-out-right');
-      setTimeout(() => {
-        prevMonth();
-        setAnimationClass('slide-in-left');
-      }, 500); // Match animation duration
-    } else if (swipeDistance < -swipeThreshold) {
-      setAnimationClass('slide-out-left');
-      setTimeout(() => {
-        nextMonth();
-        setAnimationClass('slide-in-right');
-      }, 500); // Match animation duration
+  const handleSlideChange = (swiper: SwiperCore) => {
+    if (swiper.activeIndex > swiper.previousIndex) {
+      nextMonth();
+    } else {
+      prevMonth();
     }
-  };
-
-  const handleAnimationEnd = () => {
-    setAnimationClass('');
   };
 
   return (
@@ -68,28 +44,31 @@ const Calendar: React.FC<CalendarProps> = ({
         </div>
       </div>
 
-      <div
-        className={`calendar-table-container ${animationClass}`}
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
-        onAnimationEnd={handleAnimationEnd}
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        onSlideChange={handleSlideChange}
+        allowTouchMove={true} // Enable touch swipe
       >
-        <table id="calendar-table">
-          <thead>
-            <tr>
-              <th>Пн</th>
-              <th>Вт</th>
-              <th>Ср</th>
-              <th>Чт</th>
-              <th>Пт</th>
-              <th>Сб</th>
-              <th>Нд</th>
-            </tr>
-          </thead>
-          <tbody id="calendar-body">{calendarRows}</tbody>
-        </table>
-      </div>
+        <SwiperSlide>
+          <div className="calendar-table-container">
+            <table id="calendar-table">
+              <thead>
+                <tr>
+                  <th>Пн</th>
+                  <th>Вт</th>
+                  <th>Ср</th>
+                  <th>Чт</th>
+                  <th>Пт</th>
+                  <th>Сб</th>
+                  <th>Нд</th>
+                </tr>
+              </thead>
+              <tbody id="calendar-body">{calendarRows}</tbody>
+            </table>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 };
