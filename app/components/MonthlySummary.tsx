3f78a95a25897d6a757cad5bdf0a157109dad42c
BNV1990 <03nazar03@gmail.com>
2025-05-20 19:22:36 +0300

    Fix 5

--- app/components/MonthlySummary.tsx
+++ app/components/MonthlySummary.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BsClock, BsSun, BsMoon } from 'react-icons/bs';
+import React from "react";
+import { BsClock, BsSun, BsMoon } from "react-icons/bs";
 
 interface MonthlySummaryProps {
   totalHours: number;
@@ -13,11 +13,20 @@ const MonthlySummary: React.FC<MonthlySummaryProps> = ({
   nightHours,
 }) => {
   return (
-    <div className="hours-summary" style={{ marginBottom: '20px' }}>
-      <div className="hours-summary-items">
-        <div><BsClock size={18} /><span>{totalHours}</span></div>
-        <div><BsSun size={18} /><span>{dayHours}</span></div>
-        <div><BsMoon size={18} /><span>{nightHours}</span></div>
+    <div className="hours-summary" style={{ marginBottom: "20px" }}>
+      <div className="hours-summary-items pc-align-right">
+        <div>
+          <BsClock size={18} />
+          <span>{totalHours}</span>
+        </div>
+        <div>
+          <BsSun size={18} />
+          <span>{dayHours}</span>
+        </div>
+        <div>
+          <BsMoon size={18} />
+          <span>{nightHours}</span>
+        </div>
       </div>
     </div>
   );
